b13cf3eee053bc6a703d483951e6d1aaf339037b
tegarjokam <dtegarjokam354@gmail.com>
2020-02-01 07:14:00 +0900

    tambah line baru di Tegar.java

--- src/main/java/com/backend/Tegar.java
+++ src/main/java/com/backend/Tegar.java
@@ -3,5 +3,6 @@ package com.backend;
 public class Tegar {
     public static void main(String[] args) {
         System.out.println("Tegar");
+        System.out.println("Hello semuanya ini adalah uji coba menambahkan line baru untuk git");
     }
 }
