4ed59cf798245319650b4d105399a1f40bf7d462
tegarjokam <dtegarjokam354@gmail.com>
2020-02-01 11:25:25 +0900

    tambahkan Tegar.java

---
+++ src/main/java/com/backend/Tegar.java
@@ -0,0 +1,8 @@
+package com.backend;
+
+public class Tegar {
+    public static void main(String[] args) {
+        System.out.println("Tegar");
+        System.out.println("Hello semuanya ini adalah uji coba menambahkan line baru untuk git");
+    }
+}
