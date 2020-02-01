80657894edabe497a25aa3c6a9fd6ed119b82b88
tegarjokam <dtegarjokam354@gmail.com>
2020-02-01 11:34:06 +0900

    tambahkan hello mas royid di Tegar.java

--- src/main/java/com/backend/Tegar.java
+++ src/main/java/com/backend/Tegar.java
@@ -4,5 +4,6 @@ public class Tegar {
     public static void main(String[] args) {
         System.out.println("Tegar");
         System.out.println("Hello semuanya ini adalah uji coba menambahkan line baru untuk git");
+        System.out.println("Hello mas rosyid");
     }
 }
